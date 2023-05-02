ad486ad925b5fa934afea3d401530956ace36262
marcosarias21 <marcosariasolaiz@gmail.com>
2023-05-02 19:14:12 -0300

    now if the post has like the button change

--- src/components/ButtonLike/ButtonLike.js
+++ src/components/ButtonLike/ButtonLike.js
@@ -2,16 +2,17 @@ import {
   ListItemButton, ListItemIcon, ListItemText, Typography,
 } from '@mui/material';
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 
 const ButtonLike = ({ likes, onClick, userLikeValidation }) => {
   console.log(likes);
   return (
     <ListItemButton onClick={onClick} sx={userLikeValidation ? { color: 'green' } : { color: 'gray' } }>
       <ListItemIcon sx={{ minWidth: 0 }}>
-        <ThumbUpOffAltIcon />
+        {userLikeValidation ? <ThumbUpAltIcon color='primary' /> : <ThumbUpOffAltIcon /> }
       </ListItemIcon>
         <ListItemText>
-          <Typography>{likes?.length} likes</Typography>
+          <Typography color={userLikeValidation ? 'primary' : ''} fontWeight={userLikeValidation ? 'bold' : 'normal'}>{likes?.length} likes</Typography>
         </ListItemText>
     </ListItemButton>
   );
