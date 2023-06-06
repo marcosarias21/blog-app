d340b5ba247c82af1f929dd24d8866b1222b85e1
marcosarias21 <marcosariasolaiz@gmail.com>
2023-06-06 10:34:29 -0300

    better way to parse text and return a new array with the message parse

--- src/hooks/useParseCommentsText.js
+++ src/hooks/useParseCommentsText.js
@@ -5,19 +5,23 @@ import { stateToHTML } from 'draft-js-export-html';
 
 const useParseCommentsText = (comments) => {
   const [commentsRaw, setCommentsRaw] = useState([]);
-
   useEffect(() => {
     if (comments && comments.length > 0) {
       const rawContentStates = comments.map((comment) => {
-        const parsedComment = JSON.parse(comment);
+        const parsedComment = JSON.parse(comment.message);
+        console.log(parsedComment);
         return convertFromRaw(parsedComment);
       });
       const htmls = rawContentStates.map((rawContentState) => {
         return stateToHTML(rawContentState);
       });
-      console.log(htmls);
       const htmlsParse = htmls.map(parse);
-      setCommentsRaw(htmlsParse);
+      setCommentsRaw(comments.map((html, i) => {
+        return {
+          ...html,
+          message: htmlsParse[i],
+        };
+      }));
     } else {
       setCommentsRaw([]);
     }
